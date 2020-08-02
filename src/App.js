951e85232ceaafd83fa3c7f31e2684cd48e8ff4a
Ruairi <r.patterson142@googlemail.com>
2020-08-02 16:37:08 +0100

    User can now add items to list, Additional state of "IsComplete" added (in progress)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,76 @@
-import React from 'react';
+import React, { useState } from "react";
+import "./App.css";
 import logo from './logo.svg';
 import './App.css';
 
+const Todo = ({ todo }) => <div className="todo">{todo.text}</div>;
+
+function TodoForm ( {addTodo }) {
+const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="input"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+
+}
+
+
 function App() {
-  const [todos, setTodos] = React.useState([
-    { text: "Learn about React" },
-    { text: "Meet friend for lunch" },
-    { text: "Build really cool todo app" },
-    { text: "Finish my tutorial" }
+
+  const [todos, setTodos] = useState([
+    { 
+      text: "Learn about React",
+      isCompleted: false 
+    },
+    { 
+      text: "Meet friend for lunch",
+      isCompleted: false 
+    },
+    { 
+      text: "Build really cool todo app",
+      isCompleted: false 
+    },
+    { 
+      text: "Finish my tutorial",
+      isCompleted: false 
+    }
   ]);
 
+  const addTodo = text => {
+    const newTodos = [...todos, { text }];
+    setTodos(newTodos);
+  };
+
   // we'll render our todos here ...
-  // return <div></div>
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, index) => (
+          <Todo
+            key={index}
+            index={index}
+            todo={todo}
+          />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+
 }
 
 // function App() {
